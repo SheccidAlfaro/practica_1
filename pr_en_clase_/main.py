57fdddfc0f4f031caba76f9dd1e773a1eda00807
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-25 20:43:19 -0600

    Add files via upload

---
+++ pr_en_clase_/main.py
@@ -0,0 +1,12 @@
+print("")
+print("Alfaro Ibarra Sheccid 3W, multiplicacion  de tres numeros.")
+#Definir variables y decirle al usuario que incerte numeros
+numero1=float(input("Pon el primer numero:"))
+numero2=float(input("Pon el segundo numero:"))
+numero3=float(input("Pon el tercer numero:"))
+#En una variabe se pone la multiplicacion de los tres numeros soicitados
+mult=numero1*numero2*numero3
+#Se muestra en pantalla el resultado
+print("el resultado es:")
+print(mult)
+print("")
